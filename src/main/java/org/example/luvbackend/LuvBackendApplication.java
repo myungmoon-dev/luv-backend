8f611c316f567f1cb15d3116cf6d011643b09393
wannabeing <wannabeing@naver.com>
2025-08-26 23:21:25 +0900

    feat: 명문교회 백엔드 서버 프로젝트 생성

---
+++ src/main/java/org/example/luvbackend/LuvBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.luvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuvBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuvBackendApplication.class, args);
+	}
+
+}
