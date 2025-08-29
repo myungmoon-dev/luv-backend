fb0270ff70a32ce966d0fb6f8640089d2ad63379
wannabeing <wannabeing@naver.com>
2025-08-29 19:06:54 +0900

    feat: RestClient 설정(Bean) 추가

--- src/main/java/org/example/luvbackend/exception/album/AlbumExceptionCode.java
+++ src/main/java/org/example/luvbackend/exception/album/AlbumExceptionCode.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum AlbumExceptionCode {
-	ILLEGAL_ALBUM_TYPE(false, HttpStatus.BAD_REQUEST, "존재하지 않는 앨범타입입니다.");
+	ILLEGAL_ALBUM_TYPE(false, HttpStatus.BAD_REQUEST, "존재하지 않는 앨범타입입니다."),
+	NOT_FOUND_ALBUM_OBJECT(false, HttpStatus.NOT_FOUND, "앨범을 찾을 수 없습니다."),
+	NO_IMAGE_ALBUM_FORM(false, HttpStatus.BAD_REQUEST, "이미지는 최소1개 이상 업로드해야 합니다."),
+	IMAGE_SIZE_OVER_ALBUM_FORM(false, HttpStatus.BAD_REQUEST, "이미지 크기는 10MB를 초과할 수 없습니다.");
 
 	private final boolean isSuccess;
 	private final HttpStatus httpStatus;
