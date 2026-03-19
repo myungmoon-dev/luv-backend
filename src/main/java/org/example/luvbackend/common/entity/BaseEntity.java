d919541526180a7462ab99cbc12689531f547712
wannabeing <wannabeing@naver.com>
2026-03-19 21:50:22 +0900

    Refactor: createdAt/updatedAt을 timestamp(Long)으로 변경 및 BaseEntity 통합

--- src/main/java/org/example/luvbackend/common/entity/BaseEntity.java
+++ src/main/java/org/example/luvbackend/common/entity/BaseEntity.java
@@ -1,18 +1,15 @@
 package org.example.luvbackend.common.entity;
 
-import java.time.LocalDateTime;
-
 import org.springframework.data.annotation.CreatedDate;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Getter;
 
 @Getter
 public abstract class BaseEntity {
 	@CreatedDate
-	private LocalDateTime createdAt;
+	private Long createdAt;
 
-	// @LastModifiedDate
-	// private LocalDateTime updatedAt;
+	@LastModifiedDate
+	private Long updatedAt;
 }
