076f1a14dbc25d451c06932d4095270aef446965
wannabeing <wannabeing@naver.com>
2026-03-19 21:55:21 +0900

    Feat: Bulletin(주보) 도메인 추가
     Bulletin 도메인 MVC 레이어 추가

---
+++ src/main/java/org/example/luvbackend/dto/bulletin/BulletinResponseDto.java
@@ -0,0 +1,39 @@
+package org.example.luvbackend.dto.bulletin;
+
+import java.util.List;
+
+import org.example.luvbackend.entity.bulletin.Bulletin;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class BulletinResponseDto {
+	private final String id;
+	private final String date;
+	private final String title;
+	private final List<String> imageUrls;
+	private final String createdAt;
+
+	@Builder
+	private BulletinResponseDto(String id, String date, String title, List<String> imageUrls, String createdAt) {
+		this.id = id;
+		this.date = date;
+		this.title = title;
+		this.imageUrls = imageUrls;
+		this.createdAt = createdAt;
+	}
+
+	/**
+	 * 정적 팩토리 메서드
+	 */
+	public static BulletinResponseDto from(Bulletin bulletin) {
+		return BulletinResponseDto.builder()
+				.id(bulletin.getId())
+				.date(bulletin.getDate())
+				.title(bulletin.getTitle())
+				.imageUrls(bulletin.getImageUrls())
+				.createdAt(bulletin.getFormattedCreatedAt())
+				.build();
+	}
+}
---
+++ src/main/java/org/example/luvbackend/service/BulletinService.java
@@ -0,0 +1,58 @@
+package org.example.luvbackend.service;
+
+import java.util.List;
+
+import org.example.luvbackend.dto.aws.S3Directory;
+import org.example.luvbackend.dto.bulletin.BulletinResponseDto;
+import org.example.luvbackend.dto.bulletin.BulletinUploadForm;
+import org.example.luvbackend.entity.bulletin.Bulletin;
+import org.example.luvbackend.repository.BulletinRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BulletinService {
+	private final BulletinRepository bulletinRepository;
+	private final AwsS3Service awsS3Service;
+
+	/**
+	 * 다건 페이징 주보 조회
+	 */
+	@Transactional(readOnly = true)
+	public Page<BulletinResponseDto> getBulletins(int page, int size) {
+		Pageable pageable = PageRequest.of(page, size);
+		return bulletinRepository.findAllByOrderByDateDesc(pageable)
+			.map(BulletinResponseDto::from);
+	}
+
+	/**
+	 * 단건 주보 조회
+	 */
+	@Transactional(readOnly = true)
+	public BulletinResponseDto getBulletin(String id) {
+		return BulletinResponseDto.from(bulletinRepository.findByIdOrElseThrow(id));
+	}
+
+	/**
+	 * 단건 주보 생성
+	 */
+	@Transactional
+	public BulletinResponseDto createBulletin(BulletinUploadForm form) {
+		List<String> imageUrls = awsS3Service.uploadFiles(form.getImages(), S3Directory.BULLETINS);
+		return BulletinResponseDto.from(bulletinRepository.save(Bulletin.of(form, imageUrls)));
+	}
+
+	/**
+	 * 단건 주보 삭제
+	 */
+	@Transactional
+	public void deleteBulletin(String id) {
+		bulletinRepository.delete(bulletinRepository.findByIdOrElseThrow(id));
+	}
+}
