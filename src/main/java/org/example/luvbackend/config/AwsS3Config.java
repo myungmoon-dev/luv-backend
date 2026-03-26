5f26facfa8b56d8a13f26d96091589a2df99dc07
wannabeing <wannabeing@naver.com>
2026-03-26 17:25:59 +0900

    chore: S3 접근시, EC2에 지정된 IAM Role 가져오도록 수정

--- src/main/java/org/example/luvbackend/config/AwsS3Config.java
+++ src/main/java/org/example/luvbackend/config/AwsS3Config.java
@@ -4,29 +4,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class AwsS3Config {
 
-	@Value("${spring.cloud.aws.credentials.access-key}")
-	private String accessKey;
-
-	@Value("${spring.cloud.aws.credentials.secret-key}")
-	private String secretKey;
-
 	@Value("${spring.cloud.aws.region.static}")
 	private String region;
 
 	@Bean
 	public S3Client awsS3Client() {
-		AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
 		return S3Client.builder()
 			.region(Region.of(region))
-			.credentialsProvider(StaticCredentialsProvider.create(credentials))
+			.credentialsProvider(InstanceProfileCredentialsProvider.create()) // EC2에 지정된 IAM Role 가져오기
 			.build();
 	}
 }
