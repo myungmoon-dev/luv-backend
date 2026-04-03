53bff33566aac0b1b9a72455d8767bb6ebf269f0
ChoiHyuk <wannabeing@naver.com>
2026-04-04 00:33:31 +0900

    feat: CORS 설정 추가 (#21) (#22)

---
+++ src/main/java/org/example/luvbackend/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package org.example.luvbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+			.allowedOrigins(
+				"http://localhost:3000",
+				"https://luv-renewal.netlify.app"
+			)
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
+}
