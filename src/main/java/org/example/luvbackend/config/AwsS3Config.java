5636641ce3afa96c9939da00b6f201275d6995d1
wannabeing <wannabeing@naver.com>
2026-07-05 01:32:48 +0900

    chore: AWS S3 권한 Provider 변경

--- src/main/java/org/example/luvbackend/config/AwsS3Config.java
+++ src/main/java/org/example/luvbackend/config/AwsS3Config.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -43,7 +43,7 @@ public class AwsS3Config {
 	public S3Client prodS3Client() {
 		return S3Client.builder()
 			.region(Region.of(region))
-			.credentialsProvider(InstanceProfileCredentialsProvider.create())
+			.credentialsProvider(DefaultCredentialsProvider.create())
 			.build();
 	}
 }
